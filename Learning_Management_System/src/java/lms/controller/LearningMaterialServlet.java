bf647e8600af0d33c025a58cc1291c0d46104cd3
adlynnsya <Adlynnsya2306@gmail.com>
2026-06-03 18:23:16 +0800

    create file

--- Learning_Management_System/src/java/lms/controller/LearningMaterialServlet.java
+++ Learning_Management_System/src/java/lms/controller/LearningMaterialServlet.java
@@ -18,7 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-@WebServlet("/LearningMaterialServlet")
+@WebServlet
 @MultipartConfig(
     fileSizeThreshold = 1024 * 1024 * 2,  // 2MB
     maxFileSize = 1024 * 1024 * 50,       // 50MB
